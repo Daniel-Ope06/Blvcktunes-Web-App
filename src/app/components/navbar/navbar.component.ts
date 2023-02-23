28388319cb6106c5449f71e00d531249e4438787
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-02-23 16:08:03 +0100

    create `navbar` for desktop view

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isNavbarOpen: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
   }
 
 }
